fbb003bfccded6a946332ee76f54d908ff5b7795
Karl Manu <iamkarlmburu@gmail.com>
2025-07-31 18:52:40 +0300

    Delete question3.py

--- question3.py
+++
@@ -1,7 +0,0 @@
-person_info = {}
-
-person_info['name'] = input("Enter your name: ")
-person_info['age'] = int(input("Enter your age: "))
-person_info['favourite color'] = input("Enter your favourite color: ")
-
-print(person_info)
